2364c93d3e6252f0ff759a7941a2040b77a7efb0
algoroloro <algoro9697@gmail.com>
2020-11-25 18:47:09 +0100

    celsius to farenheit

---
+++ convertCelsiusToFarenheit/README.md
@@ -0,0 +1,11 @@
+Convertir grados Celsius en Farenheit
+
+**EJEMPLO**
+
+Para celsius = -30, convertCelsiusToFarenheit(celsius) = -22.
+
+Para celsius = -20, convertCelsiusToFarenheit(celsius) = -4.
+
+Para celsius = 0, convertCelsiusToFarenheit(celsius) = 32.
+
+Para celsius = 5, convertCelsiusToFarenheit(celsius) = 41.
---
+++ convertCelsiusToFarenheit/convertCelsiusToFarenheit.ts
@@ -0,0 +1,8 @@
+function convertCelsiusToFarenheit(celsius: number): number {
+    return celsius * 9 / 5 + 32;
+}
+
+console.log(convertCelsiusToFarenheit(-20)); // DEVUELVE -4
+console.log(convertCelsiusToFarenheit(40)); // DEVUELVE 104
+console.log(convertCelsiusToFarenheit(5)); // DEVUELVE 41
+console.log(convertCelsiusToFarenheit(-30)); // DEVUELVE -22
